21cb1d8daf1f1eaeb13bed6c4c160e9a89a262cc
abiatarfestus <40578707+abiatarfestus@users.noreply.github.com>
2021-07-18 15:28:54 +0200

    Installed django-resized

--- onestop/users/models.py
+++ onestop/users/models.py
@@ -1,21 +1,24 @@
 from PIL import Image
 from django.db import models
 from django.contrib.auth.models import User
+from django_resized import ResizedImageField
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) # Delete profile when user is deleted
-    image = models.ImageField(default='profile_pics/default.png', upload_to='profile_pics')
+    image = ResizedImageField(default='profile_pics/default.png', size=[200, 200], upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile' #show how we want it to be displayed
 
     # Override the save method of the model
-    def save(self, *args, **kwargs):
-        super().save()
-        img = Image.open(self.image.path) # Open image
+    # def save(self, *args, **kwargs):
+    #     super().save()
+    #     img = Image.open(self.image.path) # Open image
+    #     # print(f'IMAGE PATH: {self.image.path}')
+    #     # print(f'IMAGE NAME: {self.image.name}')
         
-        # resize image
-        if img.height > 200 or img.width > 200:
-            output_size = (200, 200)
-            img.thumbnail(output_size) # Resize image
-            img.save(self.image.path) # Save it again and override the larger image
+    #     # resize image
+    #     if img.height > 200 or img.width > 200:
+    #         output_size = (200, 200)
+    #         img.thumbnail(output_size) # Resize image
+    #         img.save(self.image.path) # Save it again and override the larger image
