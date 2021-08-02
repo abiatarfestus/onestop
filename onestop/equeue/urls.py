da22ebcc60a2f0024122b94337782b53efed2119
Festus Abiatar <abiatarfestus@gmail.com>
2021-08-02 07:38:12 +0200

    Chenged join to form submission

--- onestop/equeue/urls.py
+++ onestop/equeue/urls.py
@@ -5,6 +5,6 @@ app_name = 'equeue'
 urlpatterns = [
     # Pass in the pk of the service_enrolment
     path('queues/<int:pk>/', views.queues, name='queues'),
-    path('queues/join-queue/<int:pk>/', views.join_queue, name='join-queue'),
+    # path('queues/join-queue/<int:pk>/', views.join_queue, name='join-queue'),
     path('services/', views.ServiceEnrolmentList.as_view(), name='services'),
 ]
--- onestop/equeue/views.py
+++ onestop/equeue/views.py
@@ -16,7 +16,7 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 
-def queues(request, pk, **kwargs):
+def queues(request, pk, join_message=None):
     '''
     Generates a view for the current queue of any particular service enrolment. Pass in the service_enrolment (pk) based on the service clicked.
 
@@ -27,27 +27,49 @@ def queues(request, pk, **kwargs):
     current_queue = QueuedCustomer.objects.filter(
         service_id=pk).order_by('join_time')
     queue_length = len(current_queue)>0
+    queued_users = [customer.customer for customer in current_queue] #List of users in queue
     context['there_is_queue'] = queue_length
     context['current_queue'] = current_queue
     context['service'] = current_service.service
+    context['service_enrolment'] = current_service
     context['service_provider'] = current_service.service_provider
     context['requirements'] = current_service.service_requirements
+    context['queued_users'] = queued_users
+    if request.method == "POST":
+        current_user = request.user
+        if current_user in queued_users:
+            join_message = 'Sorry, you\'re already queued for this service.'
+        else:
+            QueuedCustomer.objects.create(customer=current_user, service=current_service)
+            join_message = f'You\'ve been sucessfully added to the queue for {current_service.service} @ {current_service.service_provider.short_name}.'
+    context['join_message'] = join_message
     return render(request, 'equeue/queues.html', context)
 
-def join_queue(request, pk):
-    context = {}
-    current_user = request.user
-    current_service = ServiceEnrolment.objects.get(id=pk)
-    current_queue = QueuedCustomer.objects.filter(service_id=pk)
-    for customer in current_queue:
-        if customer.customer == current_user:
-            join_message= 'Sorry, you\'re already queued for this service.'
-            return queues(request=request, pk=pk, join_message=join_message)
-        else:
-            new_customer = QueuedCustomer.objects.create(customer=current_user, service=current_service)
-            join_message= 'You\'ve been sucessfully entered on to this queue.'
-    return queues(request=request, pk=pk, join_message=join_message)
-    # https://book.pythontips.com/en/latest/args_and_kwargs.html
+# def join_queue(request, pk):
+#     current_user = request.user
+#     current_service = ServiceEnrolment.objects.get(id=pk)
+#     current_queue = QueuedCustomer.objects.filter(service_id=pk)
+#     queued_users = [customer.customer for customer in current_queue] #List of users in queue
+#     if current_user in queued_users:
+#         join_message = 'Sorry, you\'re already queued for this service.'
+#         return queues(request=request, pk=pk, join_message=join_message)
+#     else:
+#         QueuedCustomer.objects.create(customer=current_user, service=current_service)
+#         join_message = f'You\'ve been sucessfully added to the queue for {current_service.service} @ {current_service.service_provider.short_name}.'
+#         print(join_message)
+#         return queues(request=request, pk=pk, join_message=join_message)
+
+    # print('Outside forloop')
+    # for customer in current_queue:
+    #     print('Entered forloop')
+    #     if customer.customer == current_user:
+    #         join_message = 'Sorry, you\'re already queued for this service.'
+    #         return queues(request=request, pk=pk, join_message=join_message)
+    #     else:
+    #         new_customer = QueuedCustomer.objects.create(customer=current_user, service=current_service)
+    #         join_message = f'You\'ve been sucessfully added to the queue for {current_service.service} @ {current_service.service_provider.short_name}.'
+    #         print(join_message)
+    #         return queues(request=request, pk=pk, join_message=join_message)
 
 
 class ServiceEnrolmentList(generic.ListView):
