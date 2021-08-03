0698bc3c28c163daf3c11db0c9138d3199d2a20b
Festus Abiatar <abiatarfestus@gmail.com>
2021-08-03 07:21:29 +0200

    Started exit_queue function

--- onestop/equeue/views.py
+++ onestop/equeue/views.py
@@ -25,9 +25,9 @@ def queues(request, pk, join_message=None):
     # Return a queryset of customers queued up for the passed in service_enrolment
     current_service = ServiceEnrolment.objects.get(id=pk)
     current_queue = QueuedCustomer.objects.filter(
-        service_id=pk).order_by('join_time')
+        service_id=pk).order_by('join_time') #A queryset of queuedCustomer instances filtered by the service_id
     queue_length = len(current_queue)>0
-    queued_users = [customer.customer for customer in current_queue] #List of users in queue
+    queued_users = [customer.customer for customer in current_queue] # #A list of user objects in current_queue
     context['there_is_queue'] = queue_length
     context['current_queue'] = current_queue
     context['service'] = current_service.service
@@ -38,38 +38,27 @@ def queues(request, pk, join_message=None):
     if request.method == "POST":
         current_user = request.user
         if current_user in queued_users:
-            join_message = 'Sorry, you\'re already queued for this service.'
+            join_message = 'Duplicate'
+            # join_message = 'Sorry, you\'re already queued for this service.'
         else:
             QueuedCustomer.objects.create(customer=current_user, service=current_service)
-            join_message = f'You\'ve been sucessfully added to the queue for {current_service.service} @ {current_service.service_provider.short_name}.'
+            join_message = "Success"
+            # join_message = f'You\'ve been sucessfully added to the queue for {current_service.service} @ {current_service.service_provider.short_name}.'
     context['join_message'] = join_message
     return render(request, 'equeue/queues.html', context)
 
-# def join_queue(request, pk):
-#     current_user = request.user
-#     current_service = ServiceEnrolment.objects.get(id=pk)
-#     current_queue = QueuedCustomer.objects.filter(service_id=pk)
-#     queued_users = [customer.customer for customer in current_queue] #List of users in queue
-#     if current_user in queued_users:
-#         join_message = 'Sorry, you\'re already queued for this service.'
-#         return queues(request=request, pk=pk, join_message=join_message)
-#     else:
-#         QueuedCustomer.objects.create(customer=current_user, service=current_service)
-#         join_message = f'You\'ve been sucessfully added to the queue for {current_service.service} @ {current_service.service_provider.short_name}.'
-#         print(join_message)
-#         return queues(request=request, pk=pk, join_message=join_message)
-
-    # print('Outside forloop')
-    # for customer in current_queue:
-    #     print('Entered forloop')
-    #     if customer.customer == current_user:
-    #         join_message = 'Sorry, you\'re already queued for this service.'
-    #         return queues(request=request, pk=pk, join_message=join_message)
-    #     else:
-    #         new_customer = QueuedCustomer.objects.create(customer=current_user, service=current_service)
-    #         join_message = f'You\'ve been sucessfully added to the queue for {current_service.service} @ {current_service.service_provider.short_name}.'
-    #         print(join_message)
-    #         return queues(request=request, pk=pk, join_message=join_message)
+def exit_queue(request, pk):
+    '''
+    Retrieve an instance of QueuedCustomer where customer_id=current_user.id and service_id=pk and save it to a variable current_instance
+    Insert in the CancelledCustomer table a new entry with queue_id and customer_id of the current_instance instance
+    Delete the current_instance instance from the QueuedCustomer
+    Return to and refresh the queue page.
+    '''
+    context = {}
+    # current_user = request.user
+    current_instance = QueuedCustomer.objects.filter(service_id=pk, customer_id=request.user.id).get() 
+    CancelledCustomer.objects.create(queue_id=current_instance.id, customer_id=current_instance.customer.id, cancelled_by=request.user)
+    current_instance.delete()
 
 
 class ServiceEnrolmentList(generic.ListView):
