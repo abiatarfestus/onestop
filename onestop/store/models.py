57bab6fba9b456c384d5f11326efc26647f2467a
abiatarfestus <40578707+abiatarfestus@users.noreply.github.com>
2022-03-06 23:03:34 +0200

    Removed name and email from customer

--- onestop/store/models.py
+++ onestop/store/models.py
@@ -5,11 +5,9 @@ from django.contrib.auth.models import User
 
 class Customer(models.Model):
 	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-	name = models.CharField(max_length=200, null=True)
-	email = models.CharField(max_length=200)
 
 	def __str__(self):
-		return self.name
+		return f'{self.user.first_name} {self.user.last_name}'
 
 
 class Product(models.Model):
