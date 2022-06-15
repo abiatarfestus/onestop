ba30e04d18f03b044bc8f261b8a785f07cd0652a
abiatarfestus <40578707+abiatarfestus@users.noreply.github.com>
2022-06-16 01:33:07 +0200

    Added email verification to user registration

--- onestop/users/urls.py
+++ onestop/users/urls.py
@@ -5,4 +5,5 @@ app_name = "users"
 urlpatterns = [
     path("register/", views.register, name="register"),
     path("profile/", views.profile, name="profile"),
+    path('activate/<uidb64>/<token>/', views.ActivateAccount.as_view(), name='activate'),
 ]
--- onestop/users/views.py
+++ onestop/users/views.py
@@ -3,14 +3,22 @@ import urllib.request
 import urllib.parse
 from django.conf import settings
 from django.contrib import messages
+from django.views.generic import View
 from django.contrib.auth import login
+from django.contrib.auth.models import User
 from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
 from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.utils.encoding import force_bytes, force_str
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
 from dictionary.classes import HistoryRecord
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
 
 
 def register(request):
@@ -30,15 +38,23 @@ def register(request):
             result = json.loads(response.read().decode())
             """ End reCAPTCHA validation """
             if result["success"]:
-                user = form.save()
-                login(request, user)
-                subject = "Welcome to the community"
-                message = f"Hi {user.first_name}, \n\nThank you for registering as a contributor. \nThe site is currently being tested; hence, your feedback at this stage will be of great importance. We cannot wait to see your contribution. \n\nRegards, \nFessy"
+                user = form.save(commit=False)
+                user.is_active = False # Deactivate account till it is confirmed
+                user.save()
+                current_site = get_current_site(request)
+                subject = 'Activate Your Oshinglish Account'
+                message = render_to_string('registration/account_activation_email.html', {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token': account_activation_token.make_token(user),
+                })
                 email_from = settings.DEFAULT_FROM_EMAIL
-                recipient_list = [user.email, "abiatarfestus@outlook.com"]
+                recipient_list = [user.email]
                 send_mail(subject, message, email_from, recipient_list)
-                messages.success(request, "You have been registered successfully!")
-                return redirect(reverse("index"))
+                messages.success(request, ('Please Confirm your email to complete registration.'))
+                # return redirect(reverse("index"))
+                return redirect('login')
             else:
                 messages.error(request, "Invalid reCAPTCHA. Please try again.")
     else:
@@ -73,4 +89,21 @@ def profile(request):
     return render(request, "users/profile.html", context)
 
 
-# Comment added on 15.03.2022 to test deployment update
+class ActivateAccount(View):
+    def get(self, request, uidb64, token, *args, **kwargs):
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        if user is not None and account_activation_token.check_token(user, token):
+            user.is_active = True
+            # user.profile.email_confirmed = True
+            user.save()
+            login(request, user)
+            messages.success(request, ('Your account has been confirmed.'))
+            return redirect('index')
+        else:
+            messages.warning(request, ('The confirmation link was invalid, possibly because it has already been used.'))
+            return redirect('index')
